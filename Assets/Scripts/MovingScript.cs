ffbe5e1cd1ba07dd983ca7d4a54f63086635ffa9
Ananda Dwi Rahmawati <ananda.dwirahmawati313@gmail.com>
2021-10-13 15:51:12 +0700

    First commit, empty project and .gitignore

---
+++ Assets/Scripts/MovingScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingScript : MonoBehaviour
+{
+    public float width = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-4 * Time.deltaTime, 0, 0);
+        if (transform.position.x <= -1 * width) {
+            transform.Translate(width, 0, 0);
+        }
+    }
+}
